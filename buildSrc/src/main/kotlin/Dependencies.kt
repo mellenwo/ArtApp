3b97844d75aa9031af70f13aa64ac16a0a5576f6
mellenwo <mellenwo@gmail.com>
2020-07-21 19:07:33 -0500

    adding koin dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,6 +6,7 @@ object Versions {
     val jUnitTest = "1.1.1"
     val jUnit = "4.12"
     val espresso = "3.2.0"
+    val koin = "2.1.6"
 }
 
 object Dependencies {
@@ -16,4 +17,11 @@ object Dependencies {
     val jUnitTestLibrary = "androidx.test.ext:junit:${Versions.jUnitTest}"
     val jUnitLibrary = "junit:junit:${Versions.jUnit}"
     val espressoLibrary = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+    val koinLibrary = "org.koin:koin-core:${Versions.koin}"
+    val koinTestLibary = "org.koin:koin-test:${Versions.koin}"
+    val koinScopeLibrary = "org.koin:koin-androidx-scope:${Versions.koin}"
+    val koinViewModelLibrary = "org.koin:koin-androidx-viewmodel:${Versions.koin}"
+    val koinFragmentLibrary = "org.koin:koin-androidx-fragment:${Versions.koin}"
+    val koinExperimentalLibrary = "org.koin:koin-androidx-ext:${Versions.koin}"
 }
+
