193fc9c91a28a7be3765a13188255ba702c35380
mellenwo <mellenwo@gmail.com>
2020-07-22 18:01:47 -0500

    converted application level gradle to kotlin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,16 @@ repositories {
     mavenCentral()
 }
 
+object PluginsVersions {
+    const val GRADLE_ANDROID = "4.0.1"
+    const val KOTLIN = "1.3.72"
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:${PluginsVersions.GRADLE_ANDROID}")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${PluginsVersions.KOTLIN}")
+}
+
 
 
 
