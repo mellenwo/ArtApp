3206f0bcb9eb9b82bf968cdaffd026343825571a
Michael Ellenwood <michael.ellenwood@csgi.com>
2021-02-02 22:04:35 -0600

    - adding bottom navigation bar - top appbar

--- features/home/src/main/kotlin/com.mellenwood.dynamicfeatures.home.ui/HomeFragment.kt
+++ features/home/src/main/kotlin/com.mellenwood.dynamicfeatures.home.ui/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.mellenwood.dynamicfeatures.home.ui
 
+import android.os.Bundle
+import android.view.View
 import com.mellenwood.artapp.ArtApplication.Companion.coreComponent
 import com.mellenwood.commons.ui.base.BaseFragment
 import com.mellenwood.dynamicfeatures.home.R
@@ -10,6 +12,32 @@ import com.mellenwood.dynamicfeatures.home.ui.di.HomeModule
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
     layoutId = R.layout.fragment_home
 ) {
+
+    /**
+     * Called to have the fragment instantiate its user interface view.
+     *
+     * @param view The view returned by onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     * @see BaseFragment.onViewCreated
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupToolbar()
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        }
+    }
+
+    private fun setupBottomNavigationBar() {
+        // TODO: need to implement
+    }
+
+    private fun setupToolbar() {
+        setHasOptionsMenu(true)
+        // TODO: uncomment once nav graph is finished
+        //requireCompatActivity().setSupportActionBar(viewBinding.toolbar)
+    }
 
     override fun onInitDependencyInjection() {
         DaggerHomeComponent
